27b89f0b0849d6ce2a186750754043a10267f726
Kanika Gupta <122543216+kanikaguptakk@users.noreply.github.com>
2024-03-26 17:12:52 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+using namespace std;
 int main()
 {
-    std::cout<<"hey guys!\n";
+    cout<<"hey guys!\n";
     printf("Enter a num:");
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int value (2);
+    cout<<value;
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int value=10 ;
+    cout<<value<<endl;
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char value;
+    cout<<"pls enter a char:";
+    cin>>value;
+    cout<<"hi"<< " "<<value<<endl;
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char value;
+    cin>>value;
+    cout<<value<<endl;
+    return 0;
 }
